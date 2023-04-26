66d7b7d4a355d0269bbccb6db2dc3cb2448fa3cf
HollosT <hollos.tamas@ravensoft.hu>
2023-04-26 17:27:16 +0200

    example

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -35,6 +35,8 @@ const Home = () => {
             <Button onClick={getHPCs}> Click me! </Button>
 
             <Accordion title="Test accordion" body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
+            <Accordion isOpen title="Another test accordion" body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
+            <Accordion title="Yet another accordion" body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
         </div>
     )
 }
