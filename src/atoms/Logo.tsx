108c6d6c4e7b759cbdd722683469afbfda915231
HollosT <hollos.tamas@ravensoft.hu>
2023-05-16 15:14:14 +0200

    updatelogo

--- src/atoms/Logo.tsx
+++ src/atoms/Logo.tsx
@@ -1,7 +1,7 @@
 const Logo: React.FC = () => {
 
     return (
-        <h3 className='logo-title'>HPC Machmaker <span className='p-small logo-title--owner'>by CLAAUDIA</span></h3>
+        <h3 className='logo-title'>HPC Matchmaker <span className='p-small logo-title--owner'>by CLAAUDIA</span></h3>
     )
 }
 
